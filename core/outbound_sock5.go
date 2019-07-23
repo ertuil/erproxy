5c1789a063bb07e15400898c48d1e100377fb5cb
ertuil <ertuil@ertuil.top>
2019-07-23 20:34:07 +0800

    same bug fixed in outbound_socks

--- core/outbound_sock5.go
+++ core/outbound_sock5.go
@@ -1,32 +1,32 @@
 package core
 
 import (
-	"io"
-	"net"
-	"log"
-	"strconv"
+	"crypto/tls"
 	"erproxy/conf"
 	"erproxy/header"
-	"crypto/tls"
+	"io"
+	"log"
+	"net"
+	"strconv"
 )
 
 type sockbound struct {
-	name string
-	c conf.OutBound
+	name   string
+	c      conf.OutBound
 	server net.Conn
 }
 
-func (sb *sockbound) getserver() net.Conn{
+func (sb *sockbound) getserver() net.Conn {
 	return sb.server
 }
 
-func (sb *sockbound) init(name string, c conf.OutBound){
+func (sb *sockbound) init(name string, c conf.OutBound) {
 	sb.name = name
 	sb.c = c
 }
 
 func (sb *sockbound) start(ad header.AddrInfo) bool {
-	_,host, port, atype, _ := ad.GetInfo()
+	_, host, port, atype, _ := ad.GetInfo()
 	var server net.Conn
 	var err error
 
@@ -37,9 +37,9 @@ func (sb *sockbound) start(ad header.AddrInfo) bool {
 		c := &tls.Config{
 			InsecureSkipVerify: true,
 		}
-		server, err = tls.Dial("tcp", net.JoinHostPort(serverHost,serverPort), c)
+		server, err = tls.Dial("tcp", net.JoinHostPort(serverHost, serverPort), c)
 	} else {
-		server, err = net.Dial("tcp", net.JoinHostPort(serverHost,serverPort))
+		server, err = net.Dial("tcp", net.JoinHostPort(serverHost, serverPort))
 	}
 
 	if err != nil {
@@ -56,20 +56,20 @@ func (sb *sockbound) start(ad header.AddrInfo) bool {
 	return ret
 }
 
-func (sb *sockbound) loop(client net.Conn){
+func (sb *sockbound) loop(client net.Conn) {
 	go io.Copy(sb.server, client)
 	io.Copy(client, sb.server)
 }
 
-func (sb *sockbound) close(){
-	if sb.server !=  nil {
+func (sb *sockbound) close() {
+	if sb.server != nil {
 		sb.server.Close()
 	}
 }
 
 // Sock5Client the client for sock5
 func Sock5Client(c conf.OutBound, server net.Conn, host, port string, atype byte) bool {
-	ret := Socks5HandShake(c,server)
+	ret := Socks5HandShake(c, server)
 	if ret == false {
 		log.Println("can not connect to the next hop")
 		return false
@@ -92,25 +92,25 @@ func Sock5Client(c conf.OutBound, server net.Conn, host, port string, atype byte
 	return true
 }
 
-// Socks5HandShake is handshake 
+// Socks5HandShake is handshake
 func Socks5HandShake(c conf.OutBound, server net.Conn) bool {
 	var b [1024]byte
-	if isOutAuth(c)  {
-		server.Write([]byte{0x05,0x01,0x02})
+	if isOutAuth(c) {
+		server.Write([]byte{0x05, 0x01, 0x02})
 
 		_, err := server.Read(b[:])
 		if err != nil {
 			log.Println(err)
 			return false
 		}
-		if b[0] != 0x05 && b[1] != 0x02{
+		if b[0] != 0x05 && b[1] != 0x02 {
 			log.Println("can not read handshake response")
 			return false
 		}
 		return true
 	}
 
-	server.Write([]byte{0x05,0x01,0x00})
+	server.Write([]byte{0x05, 0x01, 0x00})
 
 	_, err := server.Read(b[:])
 	if err != nil {
@@ -126,21 +126,21 @@ func Socks5HandShake(c conf.OutBound, server net.Conn) bool {
 
 // Socks5ClientAuth is auth client
 func Socks5ClientAuth(c conf.OutBound, server net.Conn) bool {
-	b := make([]byte,0)
-	var r [1024]byte 
-	b = append(b,0x01)
-	tu,tp := getOutAuth(c)
+	b := make([]byte, 0)
+	var r [1024]byte
+	b = append(b, 0x01)
+	tu, tp := getOutAuth(c)
 	user := []byte(tu)
 	nu := byte(len(user))
 	pass := []byte(tp)
 	np := byte(len(pass))
-	b = append(b,nu)
-	b = append(b,user...)
-	b = append(b,np)
-	b = append(b,pass...)
+	b = append(b, nu)
+	b = append(b, user...)
+	b = append(b, np)
+	b = append(b, pass...)
 
 	server.Write(b)
-	_,err := server.Read(r[:])
+	_, err := server.Read(r[:])
 	if err != nil {
 		log.Println(err)
 		return false
@@ -150,7 +150,7 @@ func Socks5ClientAuth(c conf.OutBound, server net.Conn) bool {
 		log.Println("cannot read auth response")
 		return false
 	}
-	
+
 	if r[1] == 0x01 {
 		log.Println("username or password error")
 		return false
@@ -165,39 +165,59 @@ func Socks5ClientAuth(c conf.OutBound, server net.Conn) bool {
 
 // Socks5ClientConnect lalala
 func Socks5ClientConnect(server net.Conn, host, port string, atype byte) bool {
-	b := make([]byte,0)
-	log.Println("Socks Client: Try to connect to", host + ":" + port)
+	b := make([]byte, 0)
+	log.Println("Socks Client: Try to connect to", host+":"+port)
 	var ip []byte
 	var err error
-	if atype == 0x01 || atype == 0x04{
+
+	if atype == 0x01 {
 		t := net.ParseIP(host)
-		ip, err = t.MarshalText()
-		if err != nil {
-			log.Println("Socks Client: Cannot marshal ip")
+		if t == nil {
+			log.Println("SUTP Client: Cannot marshal ip")
+			server.Close()
+			return false
+		}
+		ip = t.To4()
+		if ip == nil {
+			log.Println("SUTP Client: Cannot marshal ip")
+			server.Close()
+			return false
+		}
+	} else if atype == 0x04 {
+		t := net.ParseIP(host)
+		if t == nil {
+			log.Println("SUTP Client: Cannot marshal ip")
+			server.Close()
+			return false
+		}
+		ip = t.To16()
+		if ip == nil {
+			log.Println("SUTP Client: Cannot marshal ip")
+			server.Close()
 			return false
 		}
 	} else {
 		nh := []byte{byte(len(host))}
 		ip = []byte(host)
-		ip = append(nh,ip...)
+		ip = append(nh, ip...)
 	}
 
-	p,err := strconv.Atoi(port)
+	p, err := strconv.Atoi(port)
 	if err != nil {
 		log.Println("Socks Client: Can not marshal port")
 		return false
 	}
-	var pp  [2]byte
+	var pp [2]byte
 	pp[0] = byte(p / 256)
 	pp[1] = byte(p % 256)
 
-	b = append(b,0x05, 0x01, 0x00, atype)
-	b = append(b, ip... )
-	b = append(b, pp[0],pp[1])
+	b = append(b, 0x05, 0x01, 0x00, atype)
+	b = append(b, ip...)
+	b = append(b, pp[0], pp[1])
 
 	server.Write(b)
 	var rsp [1024]byte
-	_,err = server.Read(rsp[:])
+	_, err = server.Read(rsp[:])
 
 	if err != nil {
 		log.Println("Socks Client: Error Cannot connect from server")
